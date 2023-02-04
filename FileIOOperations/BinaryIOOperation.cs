8a5b1a081ba456f43cca7b78f0c4a0d988e93d0f
patelshivani3 <shivpatel3001@gmail.com>
2023-02-04 19:29:02 +0530

    Added Binary Serialization

---
+++ FileIOOperations/BinaryIOOperation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperations
+{
+    public class BinaryIOOperation
+    {
+        //Serialize method is use for creating data
+        public static void BinaryDataSerialization()
+        {
+            try
+            {
+                string path = @"C:\Users\shivp\source\repos\FileIOOperations\FileIOOperations\Files\BinaryData.txt";
+                List<Contact> contact = new List<Contact>()
+                {
+                    new Contact() {Id = 1, Name="Yash", Age= 22},
+                    new Contact() {Id = 2, Name="Neel", Age= 24},
+                    new Contact() {Id = 3, Name="Siya", Age= 27},
+                };
+                BinaryFormatter binary = new BinaryFormatter();
+                FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+                binary.Serialize(stream, contact);
+                stream.Close();
+                Console.WriteLine("Binary Serialization Completed Succesfully");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //deserialize method is use for reading data
+        public static void BinaryDataDeSerialization()
+        {
+            try
+            {
+                string path = @"C:\Users\shivp\source\repos\FileIOOperations\FileIOOperations\Files\BinaryData.txt";
+                List<Contact> contact = new List<Contact>()
+                {
+                    new Contact() {Id = 1, Name="Yash", Age= 22},
+                    new Contact() {Id = 2, Name="Neel", Age= 24},
+                    new Contact() {Id = 3, Name="Siya", Age= 27},
+                };
+                BinaryFormatter binary = new BinaryFormatter();
+                FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+                List<Contact> listOfContact = (List<Contact>) binary.Deserialize(stream);
+                stream.Close();
+                foreach (Contact contact1 in listOfContact)
+                {
+                    Console.WriteLine(contact1);
+                }
+                Console.WriteLine("Binary Deserialization Completed Succesfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ FileIOOperations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOperations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWelcome to File IO Operation");
+            Console.WriteLine("\n1.Binary Serialization\n2.Binary DeSerialization");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                //Binary Serialization Operation
+                case 1:
+                    BinaryIOOperation.BinaryDataSerialization();
+                    break;
+                //Binary DeSerialization Operation
+                case 2:
+                    BinaryIOOperation.BinaryDataDeSerialization();
+                    break;
+                default:
+                    Console.WriteLine("Choose from given option Only");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
